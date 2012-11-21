98d5885eff522f7c9da66628142a6deebcf41e78
Adam Kis <kisferencadam@gmail.com>
2012-11-21 01:18:29 -0800

    removed unused imports

--- app/models/RandomNumber.java
+++ app/models/RandomNumber.java
@@ -1,15 +1,9 @@
 package models;
 
-import java.util.Date;
-
-import javax.persistence.Lob;
-
-import play.data.validation.MaxSize;
 import play.data.validation.Required;
 import play.modules.morphia.Model;
 
 import com.google.code.morphia.annotations.Entity;
-import com.google.code.morphia.annotations.Reference;
  
 @Entity
 public class RandomNumber extends Model {
